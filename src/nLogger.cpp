a667a810bef05c0febdbdbbfba9f76d9cfa73b8d
Mateusz 'novo' Klos <novopl@gmail.com>
2013-10-26 23:50:01 +0100

    documentation

--- src/nLogger.cpp
+++ src/nLogger.cpp
@@ -2,8 +2,9 @@
 #include <vector>
 #include <cstdio>
 
-//------------------------------------------------------------------//
-template<typename C, typename P> static 
+//----------------------------------------------------------------------------//
+/// Helper function for finding element in the container
+template<typename C, typename P> static
 typename C::iterator find( C &container, P pred ){
   return std::find_if( container.begin(), container.end(), pred );
 }
@@ -12,6 +13,13 @@ typename C::iterator find( C &container, P pred ){
 namespace novo{
   typedef int64_t Time_t;
 
+
+  //--------------------------------------------------------------------------//
+  //{{{ void add_log_output( const std::string &name, LogOut out )
+  /// Add new log output.
+  ///   @param[in]  name  The name of the new log output.
+  ///   @returns    Timezone offset in microseconds.
+  //--------------------------------------------------------------------------//
   Time_t timezone(){
     static Time_t tzOffset = 0;
     if( !tzOffset ){
@@ -29,12 +37,19 @@ namespace novo{
     }
     return tzOffset;
   }
+  //}}}-----------------------------------------------------------------------//
+  //{{{ Time_t get_time()
+  /// Get current system time
+  ///   @returns Current system time as microseconds (start point is undef, but
+  ///   relative difference will yield time in microseconds.
+  //--------------------------------------------------------------------------//
   Time_t get_time(){
     timeval tv;
     gettimeofday(&tv, 0);
     return  static_cast<Time_t>(tv.tv_sec) * 1000000 + tv.tv_usec
             + timezone();
   }
+  //}}}
 }
 
 
@@ -47,14 +62,19 @@ namespace novo{
     LogOut        out;
   };
   typedef std::vector<LogOutDesc>   LogOutVector;
-  
+
   LogOutVector  g_outs;
   LogBuffer     g_buffer;
-  
 
 
-  //------------------------------------------------------------------//
-  void add_log_output( const std::string &name, LogOut out ){ //{{{
+
+  //--------------------------------------------------------------------------//
+  //{{{ void add_log_output( const std::string &name, LogOut out )
+  /// Add new log output.
+  ///   @param[in]  name  The name of the new log output.
+  ///   @param[in]  out   The new logger output.
+  //--------------------------------------------------------------------------//
+  void add_log_output( const std::string &name, LogOut out ){
     auto it = find( g_outs, [&name]( LogOutDesc &d){
         return d.name == name;
     });
@@ -67,20 +87,35 @@ namespace novo{
         out( entry );
     }
   }
-  //}}}---------------------------------------------------------------//
-  void remove_log_output( const std::string &name ){ //{{{
+  //}}}-----------------------------------------------------------------------//
+  //{{{ void remove_log_output( const std::string &name )
+  /// Remove log output
+  ///   @param[in]  name  The name of the log output to remove.
+  //--------------------------------------------------------------------------//
+  void remove_log_output( const std::string &name ){
     auto it = find( g_outs, [&name]( LogOutDesc &d){
         return d.name == name;
     });
     if( it != g_outs.end() )
       g_outs.erase( it );
   }
-  //}}}---------------------------------------------------------------//
-  void log( int type, const std::string &msg ){ //{{{
+  //}}}-----------------------------------------------------------------------//
+  //{{{ void log( int type, const std::string &msg )
+  /// Log message.
+  ///   @param[in]  type  Message type
+  ///   @param[in]  msg   The message itself
+  //--------------------------------------------------------------------------//
+  void log( int type, const std::string &msg ){
     lograw( type, get_time(), msg );
   }
-  //}}}---------------------------------------------------------------//
-  void lograw( int type, Time_t when, const std::string &msg ){ //{{{
+  //}}}-----------------------------------------------------------------------//
+  //{{{ void lograw( int type, Time_t when, const std::string &msg )
+  /// Log raw message.
+  ///   @param[in]  type  Message type
+  ///   @param[in]  when  Message timestamp
+  ///   @param[in]  msg   The message itself
+  //--------------------------------------------------------------------------//
+  void lograw( int type, Time_t when, const std::string &msg ){
     LogMsg  message{ type, when, msg };
     for( const auto &out: g_outs )
       out.out( message );
@@ -88,8 +123,11 @@ namespace novo{
   }
   //}}}
 
-  //------------------------------------------------------------------//
-  void stdout( const LogMsg &msg ){ //{{{
+  //--------------------------------------------------------------------------//
+  //{{{ void stdout( const LogMsg &msg )
+  /// Logger output using application standard output
+  //--------------------------------------------------------------------------//
+  void stdout( const LogMsg &msg ){
     const char *tag;
     switch( msg.type ){
       case kDebug :     tag = "debug";    break;
